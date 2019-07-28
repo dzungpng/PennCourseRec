4c073659534e6f43b82843663e06b4467abe8201
Benjamin Zhang <benjy.zhang99@gmail.com>
2019-07-28 15:39:15 -0400

    change login route

--- recommender/urls.py
+++ recommender/urls.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from .views import home, login_, signup, submit_description
 
 urlpatterns = [
-    path('login', login_, name='login'),
+    path('', login_, name='login'),
     path('signup', signup, name='signup'),
     path('home', home, name='home'),
     path('recommendation/submit/', submit_description, name='get-rec'),
